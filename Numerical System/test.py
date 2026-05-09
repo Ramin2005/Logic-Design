a2732667f3187df59f3af1abfb76c6d2398b40d1
Ramin <166108058+Ramin2005@users.noreply.github.com>
2026-05-09 22:07:04 +0330

    Update test.py

--- Numerical System/test.py
+++ Numerical System/test.py
@@ -1,9 +1,51 @@
 import code_and_number_converter
 import os
 
-os.system('cls')
-n = input("Enter number: ")
-b1 = int(input("Enter first base of number: "))
-b2 = int(input("Enter second base: "))
+n = ""
+b1 = -1
+b2 = -1
 
-print(code_and_number_converter.base_converter(n, b2, first_base=b1, floating_point=12))
+while True:
+    n = -1
+    
+    os.system('cls')
+    print("Enter negative number to exit program.")
+    n = input("Enter number: ")
+
+    if n.startswith('-'):
+        break   
+
+            
+    while b1 < 2 and b1 < 37:
+        print("Base must be between 2 to 36.")
+        
+        try:
+            b1 = int(input("Enter first base of number: "))
+
+        except ValueError:
+            os.system('cls')
+            print("Enter valid number for first base!")
+            input("Press enter to continue. ")
+            os.system('cls')
+            
+    while b2 < 2 and b2 < 37:
+        print("Base must be between 2 to 36.")
+        
+        try:
+            b2= int(input("Enter second base: "))
+
+        except ValueError:
+            os.system('cls')
+            print("Enter valid number for second base!")
+            input("Press enter to continue. ")
+            os.system('cls')
+            
+
+    os.system('cls')
+    
+    try:   
+        print(f"{n} from base {b1} to base {b2}:",code_and_number_converter.base_converter(n, b2, first_base=b1, floating_point=12))
+    except:
+        print("Entered number is invalid!")
+
+    input("Press enter to restart.")
